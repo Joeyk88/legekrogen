fe0e1f2af31cb9007942ad954a6ecc282350d021
joeykoldingkristensen <joeyk666@hotmail.com>
2024-12-09 11:23:33 +0100

    Navigation

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className='navbar'>
+      <img src='src/assets/logo/legekrogen_logo.png' alt='legekrogen_logo' />
+      <div className='burger-menu' onClick={toggleMenu}>
+        {isOpen ? <IoClose size = {36} /> : <GiHamburgerMenu size ={36} />}
+      </div>
+      <ul className={isOpen ? "nav-links open" : "nav-links"}>
+        <li>
+          <NavLink to='/'>Forside</NavLink>
+        </li>
+        <li>
+          <NavLink to='/products'>Produkter</NavLink>
+        </li>
+        <li>
+          <NavLink to='/faq'>FAQ</NavLink>
+        </li>
+        <li>
+          <NavLink to='/about'>Kundeklubben</NavLink>
+        </li>
+        <li>
+          <NavLink to='/about'>Backoffice</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
